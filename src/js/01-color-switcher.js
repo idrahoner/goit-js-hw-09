c0ab886473761bfe34bfb15bcf8d0a3c43e8813c
Ivan Drahoner <idrahoner@gmail.com>
2022-10-26 12:47:55 +0300

    Completed 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startButtonEl.addEventListener('click', onStartButtonClick);
+stopButtonEl.addEventListener('click', onStopButtonClick);
+
+stopButtonEl.disabled = 'true';
+
+function onStartButtonClick(event) {
+  stopButtonEl.removeAttribute('disabled');
+  event.currentTarget.disabled = 'true';
+
+  timerId = setInterval(switchBgColor, 1000);
+}
+
+function onStopButtonClick(event) {
+  startButtonEl.removeAttribute('disabled');
+  event.currentTarget.disabled = 'true';
+  clearInterval(timerId);
+}
+
+function switchBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
